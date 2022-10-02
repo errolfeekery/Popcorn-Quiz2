47d9ed6ff28b69519f139e5c062b7600a80f713c
errolfeekery <errolfeekery@gmail.com>
2022-10-02 23:56:48 +0000

    got choices to appear in buttons

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const startButton = document.getElementById('start-btn');
 const questionElement = document.getElementById('question')
+const btnElement1 = document.getElementById('btn1')
+const btnElement2 = document.getElementById('btn2')
+const btnElement3 = document.getElementById('btn3')
+const btnElement4 = document.getElementById('btn4')
+
+/*
 const choices = Array.from(document.querySelectorAll('.answer-buttons'))
 
 console.log(choices)
-
+*/
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
@@ -13,25 +19,30 @@ function startGame () {
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     setNextQuestion ()
-    setNextChoices()
 }
 function setNextQuestion () {
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+
 function showQuestion (question) {
     questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const p = document.createElement('p')
-        p.innerText = answer.text
-        choices.forEach(choice => {
-            const number = choice.dataset['number']
-            choice.innerText = setNextChoices['p' + 'number']
-        }
-        
-    )
+   
+        btnElement1.innerText = question.answers[0].text
+        btnElement2.innerText = question.answers[1].text
+        btnElement3.innerText = question.answers[2].text
+        btnElement4.innerText = question.answers[3].text
+    }
 
-
+/*
+question.answers.forEach(answer => {
+    const p = document.createElement('p')
+    p.innerText = answer.text
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = setNextChoices['p' + 'number']
+    }
+  */  
 /*
 function setNextChoices (answers) {
     const number = choice.dataset['number']

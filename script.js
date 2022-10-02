6abe04a4235b5738a3f2f8731f323ac05ef88f5b
errolfeekery <errolfeekery@gmail.com>
2022-10-02 13:08:13 +0000

    add js, questions not working

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const startButton = document.getElementById("start-btn");
+const startButton = document.getElementById('start-btn');
+const questionElement = document.getElementById('question')
+const answerElement = document.getElementById('answer-buttons')
 
 startButton.addEventListener('click', startGame);
 
 function startGame () {
-    console.log("hello");
+    nextQuestion ()
 }
 
-function nextQuestion () {
-
+function nextQuestion (questions) {
+    questionElement.innerText = question.questions
 }
 
 function giveAnswer () {
 
 }
+
+const questions = [
+    {
+    question: "The code in The Martrix comes from what food recipes?",
+    answers: [
+        {text: "Sushi recipes", isCorrect: true},
+        {text: "Dumpling recipes", isCorrect: false},
+        {text: "Stir-fry recipes", isCorrect: false},
+        {text: "Pad thai recpies", isCorrect: false}
+        ]
+    }
+]

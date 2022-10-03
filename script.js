21cbc41aeba57ec2b64a25099abc7ed51803dbc5
errolfeekery <errolfeekery@gmail.com>
2022-10-03 11:50:18 +0000

    add rest of questions

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const choices = Array.from(document.querySelectorAll('.answer-buttons'))
 
 console.log(choices)
 */
+
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
@@ -32,7 +33,105 @@ function showQuestion (question) {
         btnElement2.innerText = question.answers[1].text
         btnElement3.innerText = question.answers[2].text
         btnElement4.innerText = question.answers[3].text
+
+        btnElement1.value = question.answers[0].isCorrect
+        btnElement2.value = question.answers[1].isCorrect
+        btnElement3.value = question.answers[2].isCorrect
+        btnElement4.value = question.answers[3].isCorrect
+
+        }
+
+    var selected = ""
+
+    btnElement1.addEventListener('click', () => {
+        selected = btnElement1.value
+        if (selected == true) {
+            btnElement1.style.color = "green"
+        }
+    })
+
+    btnElement2.addEventListener('click', () => {
+        selected = btnElement2.value
+        if (selected == true) {
+            btnElement2.style.color = "green"
+        }
+    })
+
+    btnElement3.addEventListener('click', () => {
+        selected = btnElement3.value
+        if (selected == true) {
+            btnElement3.style.color = "green"
+        }
+    })
+
+    btnElement4.addEventListener('click', () => {
+        selected = btnElement4.value
+        if (selected == true) {
+            btnElement4.style.color = "green"
+        }
+    })
+
+    console.log(selected)
+
+
+const questions = [ 
+    {
+    question: "The code in The Martrix comes from what food recipes?",
+    answers: [
+        {text: "Sushi recipes", isCorrect: true},
+        {text: "Dumpling recipes", isCorrect: false},
+        {text: "Stir-fry recipes", isCorrect: false},
+        {text: "Pad thai recpies", isCorrect: false}
+        ]
+    },
+    {
+    question: "Where were The Lord of the Rings movies filmed?",
+    answers: [
+        {text: "Ireland", isCorrect: false},
+        {text: "Iceland", isCorrect: false},
+        {text: "New Zealand", isCorrect: true},
+        {text: "Australia", isCorrect: false}
+        ]
+    },
+    {
+    question: "If you were to watch the Marvel movies in chronological order, which movie would you watch first?",
+    answers: [
+        {text: "Iron man", isCorrect: false},
+        {text: "Captain America: The First Avenger", isCorrect: true},
+        {text: "Doctor Strange", isCorrect: false},
+        {text: "Thor", isCorrect: false}
+        ]
+    },
+    {
+    question: "Which movie is this quote from: 'Here's looking at you kid'",
+    answers:  [
+        {text: "Breakfast at Tiffany's", isCorrect: false},
+        {text: "Citizen Kane", isCorrect: false},
+        {text: "Casablanca", isCorrect: true},
+        {text: "Notorious", isCorrect: false}
+    ]  
+    },
+    {
+    question: "Which famous line from Jaws was ad-libbed?",
+    answers:  [
+        {text: "'I use to hate the water'", isCorrect: false},
+        {text: "'Your gonna need a bigger boat'", isCorrect: true},
+        {text: "'All this machine does is swim, and eat, and make little sharks'", isCorrect: false},
+        {text: "'You know, a thing about a shark, hes got lifeless eyes. Black eyes, like a dolls eyes'", isCorrect: false} 
+    ]  
+    },
+    {
+    question: "Who played Wendy Torrence in The Shining?",
+    answers: [
+        {text: "Cher", isCorrect: false},
+        {text: "Sissy Spacek", isCorrect: false},
+        {text: "Shelley Duvall", isCorrect: true},
+        {text: "Linda Ronstadt", isCorrect: false}
+    ]
     }
+
+
+]
 
 /*
 question.answers.forEach(answer => {
@@ -61,18 +160,3 @@ choices.forEach(choice => {
     choice.innerText = showQuestion['choice' + 'number']
 })
 */
-function giveAnswer () {
-
-}
-
-const questions = [ 
-    {
-    question: "The code in The Martrix comes from what food recipes?",
-    answers: [
-        {text: "Sushi recipes", isCorrect: true},
-        {text: "Dumpling recipes", isCorrect: false},
-        {text: "Stir-fry recipes", isCorrect: false},
-        {text: "Pad thai recpies", isCorrect: false}
-        ]
-    }
-]

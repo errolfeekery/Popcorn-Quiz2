1b7aefada0017fbb1ed472c9f317b886981e586d
errolfeekery <errolfeekery@gmail.com>
2022-10-04 13:11:49 +0000

    questions not appearing

--- myScript.js
+++ myScript.js
@@ -2,6 +2,68 @@ var quizContainer = document.getElementById('quiz')
 var resultsContainer = document.getElementById('results')
 var submitButton = document.getElementById('submit')
 
+var myQuestions = [
+    {
+    question: "The code in The Martrix comes from what food recipes?",
+    answers: {
+        a: "Sushi recipes", 
+        b: "Dumpling recipes",
+        c: "Stir-fry recipes", 
+        d: "Pad thai recpies", 
+    },
+
+    correctAnswer: 'b'
+
+},
+{
+    question: "Where were The Lord of the Rings movies filmed?",
+    answers: {
+        a: "Ireland",
+        b: "Iceland",
+        c: "New Zealand", 
+        d: "Australia", 
+    },
+
+    correctAnswer: 'c'
+
+},
+{
+    question: "If you were to watch the Marvel movies in chronological order, which movie would you watch first?",
+    answers: {
+        a: "Iron man", 
+        b: "Captain America: The First Avenger",
+        c: "Doctor Strange",
+        d: "Thor",
+    },
+
+    correctAnswer: 'b'
+},
+{
+    question: "Which movie is this quote from: 'Here's looking at you kid'",
+    answers:  {
+        a: "Breakfast at Tiffany's",
+        b: "Citizen Kane", 
+        c: "Casablanca", 
+        d: "Notorious", 
+    },
+
+    correctAnswer: 'c'
+    
+},
+{
+    question: "Which famous line from Jaws was ad-libbed?",
+    answers:  {
+        a: "'I use to hate the water'", 
+        b: "'Your gonna need a bigger boat'",
+        c: "'All this machine does is swim, and eat, and make little sharks'", 
+        d: "'You know, a thing about a shark, hes got lifeless eyes. Black eyes, like a dolls eyes'", 
+    }, 
+
+    correctAnswer: 'b'
+    }
+    
+];
+
 generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton)
 
 
@@ -74,65 +136,3 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
     }
 
 }
-
-var myQuestions = [
-    {
-    question: "The code in The Martrix comes from what food recipes?",
-    answers: {
-        a: "Sushi recipes", 
-        b: "Dumpling recipes",
-        c: "Stir-fry recipes", 
-        d: "Pad thai recpies", 
-    },
-
-    correctAnswer: 'b'
-
-},
-{
-    question: "Where were The Lord of the Rings movies filmed?",
-    answers: {
-        a: "Ireland",
-        b: "Iceland",
-        c: "New Zealand", 
-        d: "Australia", 
-    },
-
-    correctAnswer: 'c'
-
-},
-{
-    question: "If you were to watch the Marvel movies in chronological order, which movie would you watch first?",
-    answers: {
-        a: "Iron man", 
-        b: "Captain America: The First Avenger",
-        c: "Doctor Strange",
-        d: "Thor",
-    },
-
-    correctAnswer: 'b'
-},
-{
-    question: "Which movie is this quote from: 'Here's looking at you kid'",
-    answers:  {
-        a: "Breakfast at Tiffany's",
-        b: "Citizen Kane", 
-        c: "Casablanca", 
-        d: "Notorious", 
-    },
-
-    correctAnswer: 'c'
-    
-},
-{
-    question: "Which famous line from Jaws was ad-libbed?",
-    answers:  {
-        a: "'I use to hate the water'", 
-        b: "'Your gonna need a bigger boat'",
-        c: "'All this machine does is swim, and eat, and make little sharks'", 
-        d: "'You know, a thing about a shark, hes got lifeless eyes. Black eyes, like a dolls eyes'", 
-    }, 
-
-    correctAnswer: 'b'
-    }
-    
-];

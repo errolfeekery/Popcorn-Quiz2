ffc601f23238b081400e67c2ebfa34879698d72f
errolfeekery <errolfeekery@gmail.com>
2022-10-04 12:04:25 +0000

    add more js to myScript

--- myScript.js
+++ myScript.js
@@ -1,10 +1,68 @@
-function generateQuiz(question, quizContainer, resultsContainer, submitButton) {
+var quizContainer = document.getElementById('quiz')
+var resultsContainer = document.getElementById('results')
+var submitButton = document.getElementById('submit')
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton)
+
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
     function showQuestions(questions, quizContainer) {
+        var output = []
+        var answers
 
+        for(var i=0; i<questions.length; i++) {
+
+            answers = []
+
+            for(letter in questions[i].answers) {
+
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    +'</label>'
+                );
+            }
+
+            output.push(
+                '<div class="question' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        quizContainer.innerHTML = output.join('')
     }
 
+
+    showQuestions(questions, quizContainer)
+
     function showResults(questions, quizContainer, resultsContainer) {
+
+        var answerContainers = quizContainer.querySelectorAll('.answers')
+
+        var userAnswer = ''
+        var numCorrect = 0
+
+        for(var i=0; i<questions.length; i++) {
+
+            userAnswer = (answerContainers[i].querySelectorAll('input[name=question'+i+']:checked')||{}).value
+
+            if(userAnswer===questions[i].correctAnswer) {
+
+                numCorrect++
+
+                answerContainers[i].style.color = 'lightgreen'
+            }
+
+            else {
+
+                answerContainers[i].style.color = 'red'
+            }
+        }
+
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length
 
     }
 
@@ -14,6 +72,7 @@ function generateQuiz(question, quizContainer, resultsContainer, submitButton) {
 
         showResults(questions, quizContainer, resultsContainer)
     }
+
 }
 
 var myQuestions = [
